a21a3cac04310e49fb0452583a9dfccaa15e1b4d
RichaLogiciel <richa@logiciel.com>
2024-09-25 09:29:52 +0530

    feat: button-component

---
+++ figma-design/src/components/button/button.tsx
@@ -0,0 +1,6 @@
+export default function Button() {
+    return (
+        <button className="flex justify-center bg-primary font-roboto text-white p-1  max-w-full w-screen"
+        >Button</button>
+    )
+}
